7c797392f531161c1666ef51f0ffb15710fc15e6
davidebaggio <davide.davide2405@gmail.com>
2024-06-16 19:25:32 +0200

    Updates

---
+++ precache-manifest.1670e607da2216fee9039dd1cefadbd4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3195a795dd8afd9d5262da877c994db",
+    "url": "/simple-notes/index.html"
+  },
+  {
+    "revision": "ec259569c065b12c6d2c",
+    "url": "/simple-notes/static/css/main.89473208.chunk.css"
+  },
+  {
+    "revision": "e998941e4a1076ad269c",
+    "url": "/simple-notes/static/js/2.bc46112b.chunk.js"
+  },
+  {
+    "revision": "65a9926ecde5afbe3bda",
+    "url": "/simple-notes/static/js/3.3b6fb9ae.chunk.js"
+  },
+  {
+    "revision": "ec259569c065b12c6d2c",
+    "url": "/simple-notes/static/js/main.f04ccfd5.chunk.js"
+  },
+  {
+    "revision": "26227a6d5516480ff382",
+    "url": "/simple-notes/static/js/runtime~main.80080b05.js"
+  }
+]);
